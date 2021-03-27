e37eec90787f28f8b35afa55855fdeed00746650
Eric Mesa <ericsbinaryworld@gmail.com>
2021-03-26 20:12:17 -0400

    OK, I've got a page to redirect to when I login

--- prophecypracticum/practicumweb/urls.py
+++ prophecypracticum/practicumweb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 app_name = 'practicumweb'
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='practicum/home.html'), name='home'),
     path('newprophecy/', views.new_prophecy, name="newprophecy")
 ]
