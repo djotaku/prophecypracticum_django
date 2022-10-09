ba63d914127d0e2ee9b290e9f6c13b21c3beb493
Eric Mesa <ericsbinaryworld@gmail.com>
2022-10-09 12:14:53 -0400

    fix for csrf changes in Django 4.0
    Attempt 9

--- prophecypracticum/prophecypracticum/settings.py
+++ prophecypracticum/prophecypracticum/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['prophecypracticum.ericmesa.com', '127.0.0.1']
 ALLOWED_ORIGINS = ['prophecypracticum.ericmesa.com', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://*', 'http://localhost', 'http://127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://*', 'http://localhost', 'http://127.0.0.1', 'http://prophecypracticum.ericmesa.com']
 # CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
 
 # Application definition
